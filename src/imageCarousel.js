3b41c6bd2bfdb4888399cbe348cd62cae50ae50d
aderose <sandro.derose28@yahoo.com>
2020-11-05 19:40:58 +0000

    Implement image carousel image switch animation

--- src/imageCarousel.js
+++ src/imageCarousel.js
@@ -4,11 +4,31 @@ export default function imageCarousel({ containerSelector, images }) {
   const container = document.querySelector(containerSelector);
   const slides = images.map((img) => slide.create(img));
   let iterator = 0;
+  let nextSlide = slides[0];
+  let direction = 'next';
 
-  // empty the container and show the new slide
-  function showSlide(slideObj) {
+  // remove classes when picture is active
+  function active() {
+    this.setAttribute('class', '');
+    this.removeEventListener('animationend', active);
+  }
+
+  function showNextSlide() {
+    // remove the animation class & listener from the previous element
+    this.setAttribute('class', '');
+    this.removeEventListener('animationend', showNextSlide);
+
+    // clear container, append the next slide with animation
     container.innerHTML = '';
-    container.appendChild(slideObj);
+    nextSlide.addEventListener('animationend', active);
+    nextSlide.setAttribute('class', direction);
+    container.appendChild(nextSlide);
+  }
+
+  function switchSlide() {
+    const previous = container.firstChild;
+    previous.addEventListener('animationend', showNextSlide);
+    previous.setAttribute('class', `before-${direction}`);
   }
 
   // compute the modulus of +ve & -ve numbers
@@ -19,17 +39,21 @@ export default function imageCarousel({ containerSelector, images }) {
   // move forwards, loop back to first slide at the end
   function moveForward() {
     iterator = mod(iterator + 1, images.length);
-    showSlide(slides[iterator]);
+    nextSlide = slides[iterator];
+    direction = 'next';
+    switchSlide();
   }
 
   // move backwards, loop back to last slide at the start
   function moveBackward() {
     iterator = mod(iterator - 1, images.length);
-    showSlide(slides[iterator]);
+    nextSlide = slides[iterator];
+    direction = 'previous';
+    switchSlide();
   }
 
   // initialise carousel with the first slide
-  showSlide(slides[0]);
+  container.appendChild(nextSlide);
 
   return { moveForward, moveBackward };
 }
