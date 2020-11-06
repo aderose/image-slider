f6d62dbe04d50eb3302a119ef8d0fb39b125e7e9
aderose <sandro.derose28@yahoo.com>
2020-11-06 20:39:53 +0000

    Add the activate & deactivate methods to animate slide switching

--- src/slide.js
+++ src/slide.js
@@ -1,9 +1,29 @@
+import navLink from './navLink';
+
 export default (function slide() {
-  function create({ src, alt }) {
+  function create({ src, alt }, id) {
+    const slideLink = navLink.create(id);
     const image = document.createElement('img');
     image.setAttribute('src', src);
     image.setAttribute('alt', alt);
-    return image;
+    image.setAttribute('id', id);
+
+    async function activate(direction) {
+      slideLink.updateStatus({ isActive: true });
+      image.setAttribute('class', direction);
+      // reset the image class to blank after the animation duration
+      await new Promise((r) => setTimeout(r, 500));
+      image.setAttribute('class', '');
+    }
+
+    async function deactivate(direction) {
+      slideLink.updateStatus({ isActive: false });
+      image.setAttribute('class', `before-${direction}`);
+      await new Promise((r) => setTimeout(r, 200));
+      image.setAttribute('class', '');
+    }
+
+    return { image, slideLink, activate, deactivate };
   }
   return { create };
 })();
